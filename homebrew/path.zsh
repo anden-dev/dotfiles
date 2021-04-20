0d370a5b9186e2832fa1f72bd53b97f56ed5e533
Andreas Engl <Andreas.Engl@vodafone.com>
2021-04-20 16:04:48 +0200

    feat(hombrew): fixup path

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,17 +1,9 @@
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/node@10/bin/node:$PATH"
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/krb5/bin:$PATH"
-export PATH="/usr/local/opt/krb5/sbin:$PATH"
-export PATH="/usr/local/opt/node@10/bin:$PATH"
-export PATH="/usr/local/opt/ruby/bin:$PATH"
-export PATH="$(gem environment gemdir)/bin:$PATH"
-export PATH="/usr/local/opt/zip/bin:$PATH"
-export PATH="/usr/local/opt/curl/bin:$PATH"
-
-export PATH="/usr/local/opt/util-linux/bin:$PATH"
-export PATH="/usr/local/opt/util-linux/sbin:$PATH"
+export PATH="$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix)/opt/make/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix)/opt/zip/bin:$PATH"
+export PATH="$(brew --prefix)/opt/curl/bin:$PATH"
+export PATH="$(brew --prefix)/opt/util-linux/bin:$PATH"
+export PATH="$(brew --prefix)/opt/util-linux/sbin:$PATH"
 
 for d in $(brew --prefix)/opt/*/libexec/gnubin; do
     export PATH=$d:$PATH;
