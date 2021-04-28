5afcffa0372e9901062a520f962aec4a25031854
Andreas Engl <Andreas.Engl@vodafone.com>
2021-04-28 17:05:30 +0200

    feat(system): use exa as ls replacement

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,12 +1,24 @@
 #!/bin/sh
-if [ "$(uname -s)" = "Darwin" ]; then
-	alias ls="ls -FG"
-else
-	alias ls="ls -F --color"
-fi
-alias l="ls -lAh"
-alias la="ls -A"
-alias ll="ls -l"
+
+
+# general use
+alias ls='exa'                                                          # ls
+alias l='exa -lbF --git'                                                # list, size, type, git
+alias ll='exa -lbGF --git'                                             # long list
+alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+
+# specialty views
+alias lS='exa -1'                                                              # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
+alias tree="exa --tree -F"
+
+
+alias cat='bat --style=header,grid'
+
+alias t='tail -f'
+
 
 alias grep="grep --color=auto"
 alias duf="du -sh * | sort -hr"
