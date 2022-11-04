5cbb936cc2d041465d667e3323ecb5fe02dd0a86
Andreas Engl <Andreas.Engl@vodafone.com>
2022-11-04 18:33:55 +0100

    feat(git): add alias to handle local branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,6 +3,7 @@ if command -v hub >/dev/null 2>&1; then
 	alias git='hub'
 fi
 
+
 alias gl='git pull --prune'
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
 alias glga="glg --all"
@@ -29,3 +30,16 @@ fi
 gi() {
 	curl -s "https://www.gitignore.io/api/$*"
 }
+
+alias git='nocorrect git'
+
+alias gcdroot='pushd $(git root)'
+
+
+#alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
+
+alias gbda_merged='git branch --merged | grep -vE "(^\*|master|dev)" | xargs git branch -d'
+# delete all local unmerged branches
+alias gbda_unmerged='git branch --no-merged | grep -vE "(^\*|master|dev)" | xargs git branch -D'
+# delete all local branches (merged and unmerged).
+alias gbda_hold_my_beer='git branch | grep -vE "(^\*|master|dev)" | xargs git branch -D'
