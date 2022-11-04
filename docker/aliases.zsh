3d6dfc619e6d2644ed0bb24d3e2c3b872ca702fe
Andreas Engl <Andreas.Engl@vodafone.com>
2022-11-04 18:30:10 +0100

    feat(docker): use colima instead of docker desktop

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -2,3 +2,10 @@
 docker_prune() {
 	docker system prune --volumes -fa
 }
+
+docker_up() {
+	colima start
+}
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
