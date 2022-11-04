fe27573b8c38220881ed31a37163a3e35f49c5b6
Andreas Engl <Andreas.Engl@vodafone.com>
2022-11-04 18:38:17 +0100

    feat(terraform): add alias tf and tfproviders

---
+++ terraform/aliases.zsh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+alias tf="terraform"
+alias tfproviders="rm terraform.locks.hcl && terraform providers lock -platform=linux_arm64 -platform=linux_amd64 -platform=darwin_amd64 -platform=darwin_arm64 -platform=windows_amd64"
