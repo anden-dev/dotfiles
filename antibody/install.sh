887db7d893ffc1693950f79d5ec480923cd2c6e9
Andreas Engl <Andreas.Engl@vodafone.com>
2023-02-13 17:49:40 +0100

    feat(antibody): use dir

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink,
+    # we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+
 if ! hash antibody 2>/dev/null; then
-	brew install antibody
+    brew install antibody
 fi
 
-antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+antibody bundle <"$DIR/bundles.txt" >"$HOME/.zsh_plugins.sh"
 antibody update
