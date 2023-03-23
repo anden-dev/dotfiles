b5429c92f24dd700457b809549763cbfe029572c
Andreas Engl <Andreas.Engl@vodafone.com>
2023-03-23 13:55:35 +0100

    feat(brew): update brewfile

--- homebrew/autoupdate.zsh
+++ homebrew/autoupdate.zsh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export HOMEBREW_NO_AUTO_UPDATE=1
 
 #export HOMEBREW_AUTO_UPDATE_SECS="86400"
 #uncomment to disable
-#export HOMEBREW_NO_AUTO_UPDATE="1"
+export HOMEBREW_NO_AUTO_UPDATE="1"
